637f0147fa086e8da62068a49983e2ce951b877e
嘲讽 <112706806+sneer0@users.noreply.github.com>
2022-09-29 19:48:03 +0800

    Update 下架歌曲.lua

--- 留声机App/下架歌曲.lua
+++ 留声机App/下架歌曲.lua
@@ -426,215 +426,215 @@ list.onItemClick=function(adp,view,pos,id)
     end,--结束●投稿下架歌曲结束●
 
     function()--●1●
-      进入子页面("调用",{标题="01《未分类》",链接="https://sneer.lanzoui.com/b06t7ouvi"}) 
+      进入子页面("调用",{标题="01《未分类》",链接="https://lanzoui.com/b06t7ouvi"}) 
     end,--结束●1●
 
     function()--●2●
-      进入子页面("调用",{标题="02《崔健》",链接="https://sneer.lanzoui.com/b06t7n6vi"})
+      进入子页面("调用",{标题="02《崔健》",链接="https://lanzoui.com/b06t7n6vi"})
     end,--结束●2●
 
     function()--●3●
-      进入子页面("调用",{标题="03《刘森》",链接="https://sneer.lanzoui.com/b06t7n80j"})
+      进入子页面("调用",{标题="03《刘森》",链接="https://lanzoui.com/b06t7n80j"})
     end,--结束●3●
 
     function()--●4●
-      进入子页面("调用",{标题="04《老狼》",链接="https://sneer.lanzoui.com/b06t7qtha"})
+      进入子页面("调用",{标题="04《老狼》",链接="https://lanzoui.com/b06t7qtha"})
     end,--结束●4●
 
     function()--●5●
-      进入子页面("调用",{标题="05《伍佰》",链接="https://sneer.lanzoui.com/b06t7qtle"})
+      进入子页面("调用",{标题="05《伍佰》",链接="https://lanzoui.com/b06t7qtle"})
     end,--结束●5●
 
     function()--●6●
-      进入子页面("调用",{标题="06《红色摇滚》",链接="https://sneer.lanzoui.com/b06t7qtud"})
+      进入子页面("调用",{标题="06《红色摇滚》",链接="https://lanzoui.com/b06t7qtud"})
     end,--结束●6●
 
     function()--●7●
-      进入子页面("调用",{标题="07《万能青年旅店》",链接="https://sneer.lanzoui.com/b06t7qtzi"})
+      进入子页面("调用",{标题="07《万能青年旅店》",链接="https://lanzoui.com/b06t7qtzi"})
     end,--结束●7●
 
     function()--●8●
-      进入子页面("调用",{标题="08《现代人乐队》",链接="https://sneer.lanzoui.com/b06t7vanc"}) 
+      进入子页面("调用",{标题="08《现代人乐队》",链接="https://lanzoui.com/b06t7vanc"}) 
     end,--结束●8●
 
     function()--●9●
-      进入子页面("调用",{标题="09《花粥》",链接="https://sneer.lanzoui.com/b06t7vdna"})
+      进入子页面("调用",{标题="09《花粥》",链接="https://lanzoui.com/b06t7vdna"})
     end,--结束●9●
 
     function()--●10●
-      进入子页面("调用",{标题="10《GALA》",链接="https://sneer.lanzoui.com/b06t7ve6j"})
+      进入子页面("调用",{标题="10《GALA》",链接="https://lanzoui.com/b06t7ve6j"})
     end,--结束●10●
 
     function()--●11●
-      进入子页面("调用",{标题="11《王洛宾》",链接="https://sneer.lanzoui.com/b06t7vera"})
+      进入子页面("调用",{标题="11《王洛宾》",链接="https://lanzoui.com/b06t7vera"})
     end,--结束●11●
 
     function()--●12●
-      进入子页面("调用",{标题="12《赵雷》",链接="https://sneer.lanzoui.com/b06t7vfih"})
+      进入子页面("调用",{标题="12《赵雷》",链接="https://lanzoui.com/b06t7vfih"})
     end,--结束●12●
 
     function()--●13●
-      进入子页面("调用",{标题="13《侃侃》",链接="https://sneer.lanzoui.com/b06t7vfmb"})
+      进入子页面("调用",{标题="13《侃侃》",链接="https://lanzoui.com/b06t7vfmb"})
     end,--结束●13●
 
     function()--●14●
-      进入子页面("调用",{标题="14《好妹妹乐队》",链接="https://sneer.lanzoui.com/b06t7vfqf"})
+      进入子页面("调用",{标题="14《好妹妹乐队》",链接="https://lanzoui.com/b06t7vfqf"})
     end,--结束●14●
 
     function()--●15●
-      进入子页面("调用",{标题="15《撒娇》",链接="https://sneer.lanzoui.com/b06t7vh9a"})
+      进入子页面("调用",{标题="15《撒娇》",链接="https://lanzoui.com/b06t7vh9a"})
     end,--结束●15●
 
     function()--●16●
-      进入子页面("调用",{标题="16《宋冬野》",链接="https://sneer.lanzoui.com/b06t7vq6b"})
+      进入子页面("调用",{标题="16《宋冬野》",链接="https://lanzoui.com/b06t7vq6b"})
     end,--结束●16●
 
     function()--●17●
-      进入子页面("调用",{标题="17《晓月老板》",链接="https://sneer.lanzoui.com/b06t7vqgb"})
+      进入子页面("调用",{标题="17《晓月老板》",链接="https://lanzoui.com/b06t7vqgb"})
     end,--结束●17●
 
     function()--●18●
-      进入子页面("调用",{标题="18《尧十三》",链接="https://sneer.lanzoui.com/b06t7vqlg"})
+      进入子页面("调用",{标题="18《尧十三》",链接="https://lanzoui.com/b06t7vqlg"})
     end,--结束●18●
 
     function()--●19●
-      进入子页面("调用",{标题="19《云母逼乐队》",链接="https://sneer.lanzoui.com/b06t7vqoj"})
+      进入子页面("调用",{标题="19《云母逼乐队》",链接="https://lanzoui.com/b06t7vqoj"})
     end,--结束●19●
 
     function()--●20●
-      进入子页面("调用",{标题="20《周云蓬》",链接="https://sneer.lanzoui.com/b06t7vrpg"})
+      进入子页面("调用",{标题="20《周云蓬》",链接="https://lanzoui.com/b06t7vrpg"})
     end,--结束●20●
 
     function()--●21●
-      进入子页面("调用",{标题="21《痛仰乐队》",链接="https://sneer.lanzoui.com/b06t7vs3a"})
+      进入子页面("调用",{标题="21《痛仰乐队》",链接="https://lanzoui.com/b06t7vs3a"})
     end,--结束●21●
 
     function()--●22●
-      进入子页面("调用",{标题="22《马雨阳》",链接="https://sneer.lanzoui.com/b06t7vt0d"})
+      进入子页面("调用",{标题="22《马雨阳》",链接="https://lanzoui.com/b06t7vt0d"})
     end,--结束●22●
 
     function()--●23●
-      进入子页面("调用",{标题="23《腰乐队》",链接="https://sneer.lanzoui.com/b06t7wakf"})
+      进入子页面("调用",{标题="23《腰乐队》",链接="https://lanzoui.com/b06t7wakf"})
     end,--结束●23●
 
     function()--●24●
-      进入子页面("调用",{标题="24《黑撒乐队》",链接="https://sneer.lanzoui.com/b06t7wbij"})
+      进入子页面("调用",{标题="24《黑撒乐队》",链接="https://lanzoui.com/b06t7wbij"})
     end,--结束●24●
 
     function()--●25●
-      进入子页面("调用",{标题="25《黑豹乐队》",链接="https://sneer.lanzoui.com/b06t7wbwd"})
+      进入子页面("调用",{标题="25《黑豹乐队》",链接="https://lanzoui.com/b06t7wbwd"})
     end,--结束●25●
 
     function()--●26●
-      进入子页面("调用",{标题="26《何教授》",链接="https://sneer.lanzoui.com/b06t7wcif"})
+      进入子页面("调用",{标题="26《何教授》",链接="https://lanzoui.com/b06t7wcif"})
     end,--结束●26●
 
     function()--●27●
-      进入子页面("调用",{标题="27《许巍》",链接="https://sneer.lanzoui.com/b06t7we7g"})
+      进入子页面("调用",{标题="27《许巍》",链接="https://lanzoui.com/b06t7we7g"})
     end,--结束●27●
 
     function()--●28●
-      进入子页面("调用",{标题="28《贰佰》",链接="https://sneer.lanzoui.com/b06t7wflg"})
+      进入子页面("调用",{标题="28《贰佰》",链接="https://lanzoui.com/b06t7wflg"})
     end,--结束●28●
 
     function()--●29●
-      进入子页面("调用",{标题="29《驳倒乐队》",链接="https://sneer.lanzoui.com/b06t7wg2d"})
+      进入子页面("调用",{标题="29《驳倒乐队》",链接="https://lanzoui.com/b06t7wg2d"})
     end,--结束●29●
 
     function()--●30●
-      进入子页面("调用",{标题="30《谢天笑》",链接="https://sneer.lanzoui.com/b06t7wgzg"}) 
+      进入子页面("调用",{标题="30《谢天笑》",链接="https://lanzoui.com/b06t7wgzg"}) 
     end,--结束●30●
 
     function()--●31●
-      进入子页面("调用",{标题="31《万晓利》",链接="https://sneer.lanzoui.com/b06t7wi0d"})
+      进入子页面("调用",{标题="31《万晓利》",链接="https://lanzoui.com/b06t7wi0d"})
     end,--结束●31●
 
     function()--●32●
-      进入子页面("调用",{标题="32《陈升》",链接="https://sneer.lanzoui.com/b06t7wiqj"})
+      进入子页面("调用",{标题="32《陈升》",链接="https://lanzoui.com/b06t7wiqj"})
     end,--结束●32●
 
     function()--●33●
-      进入子页面("调用",{标题="33《罗大佑》",链接="https://sneer.lanzoui.com/b06t7wmif"})
+      进入子页面("调用",{标题="33《罗大佑》",链接="https://lanzoui.com/b06t7wmif"})
     end,--结束●33●
 
     function()--●34●
-      进入子页面("调用",{标题="34《黄耀明》",链接="https://sneer.lanzoui.com/b06t7wn7a"})
+      进入子页面("调用",{标题="34《黄耀明》",链接="https://lanzoui.com/b06t7wn7a"})
     end,--结束●34●
 
     function()--●35●
-      进入子页面("调用",{标题="35《Beyond》",链接="https://sneer.lanzoui.com/b06t7wo8h"})
+      进入子页面("调用",{标题="35《Beyond》",链接="https://lanzoui.com/b06t7wo8h"})
     end,--结束●35●
 
     function()--●36●
-      进入子页面("调用",{标题="36《周传雄》",链接="https://sneer.lanzoui.com/b06t7wola"})
+      进入子页面("调用",{标题="36《周传雄》",链接="https://lanzoui.com/b06t7wola"})
     end,--结束●36●
 
     function()--●37●
-      进入子页面("调用",{标题="37《陈一发儿》",链接="https://sneer.lanzoui.com/b06t7wrtg"})
+      进入子页面("调用",{标题="37《陈一发儿》",链接="https://lanzoui.com/b06t7wrtg"})
     end,--结束●37●
 
     function()--●38●
-      进入子页面("调用",{标题="38《许嵩》",链接="https://sneer.lanzoui.com/b06t7wrwj"})
+      进入子页面("调用",{标题="38《许嵩》",链接="https://lanzoui.com/b06t7wrwj"})
     end,--结束●38●
 
     function()--●39●
-      进入子页面("调用",{标题="39《康姆士乐团》",链接="https://sneer.lanzoui.com/b06t7x72f"})
+      进入子页面("调用",{标题="39《康姆士乐团》",链接="https://lanzoui.com/b06t7x72f"})
     end,--结束●39●
 
     function()--●40●
-      进入子页面("调用",{标题="40《张楚》",链接="https://sneer.lanzoui.com/b06t7x8hg"}) 
+      进入子页面("调用",{标题="40《张楚》",链接="https://lanzoui.com/b06t7x8hg"}) 
     end,--结束●40●
 
     function()--●41●
-      进入子页面("调用",{标题="41《后海大鲨鱼》",链接="https://sneer.lanzoui.com/b06t7wdgj"})
+      进入子页面("调用",{标题="41《后海大鲨鱼》",链接="https://lanzoui.com/b06t7wdgj"})
     end,--结束●41●
 
     function()--●42●
-      进入子页面("调用",{标题="42《张震岳》",链接="https://sneer.lanzoui.com/b06t8sl5i"})
+      进入子页面("调用",{标题="42《张震岳》",链接="https://lanzoui.com/b06t8sl5i"})
     end,--结束●42●
 
     function()--●43●
-      进入子页面("调用",{标题="43《my little airport》",链接="https://sneer.lanzoui.com/b06t8xmih"})
+      进入子页面("调用",{标题="43《my little airport》",链接="https://lanzoui.com/b06t8xmih"})
     end,--结束●43●
 
     function()--●44●
-      进入子页面("调用",{标题="44《李荣浩》",链接="https://sneer.lanzoui.com/b06t9apkf"})
+      进入子页面("调用",{标题="44《李荣浩》",链接="https://lanzoui.com/b06t9apkf"})
     end,--结束●44●
 
     function()--●45●
-      进入子页面("调用",{标题="45《任贤齐》",链接="https://sneer.lanzoui.com/b06t9arcj"})
+      进入子页面("调用",{标题="45《任贤齐》",链接="https://lanzoui.com/b06t9arcj"})
     end,--结束●45●
 
     function()--●46●
-      进入子页面("调用",{标题="46《王菲》",链接="https://sneer.lanzoui.com/b06t9b7xg"})
+      进入子页面("调用",{标题="46《王菲》",链接="https://lanzoui.com/b06t9b7xg"})
     end,--结束●46●
 
     function()--●47●
-      进入子页面("调用",{标题="47《苏打绿》",链接="https://sneer.lanzoui.com/b06tbbcte"})
+      进入子页面("调用",{标题="47《苏打绿》",链接="https://lanzoui.com/b06tbbcte"})
     end,--结束●47●
 
     function()--●48●
-      进入子页面("调用",{标题="48《国际歌》",链接="https://sneer.lanzoui.com/b06tbltzc"})
+      进入子页面("调用",{标题="48《国际歌》",链接="https://lanzoui.com/b06tbltzc"})
     end,--结束●48●
 
     function()--●49●
-      进入子页面("调用",{标题="49《野外合作社》",链接="https://sneer.lanzoui.com/b06tm5fjc"})
+      进入子页面("调用",{标题="49《野外合作社》",链接="https://lanzoui.com/b06tm5fjc"})
     end,--结束●49●
 
     function()--●50●
-      进入子页面("调用",{标题="50《爱国歌曲》",链接="https://sneer.lanzoui.com/b06tmchpc"})
+      进入子页面("调用",{标题="50《爱国歌曲》",链接="https://lanzoui.com/b06tmchpc"})
     end,--结束●50●
 
     function()--●51●
-      进入子页面("调用",{标题="51《仙剑奇侠传三》",链接="https://sneer.lanzoui.com/b06tpisbi"})
+      进入子页面("调用",{标题="51《仙剑奇侠传三》",链接="https://lanzoui.com/b06tpisbi"})
     end,--结束●51●
 
     function()--●52●
-      进入子页面("调用",{标题="52《草东没有派对》",链接="https://sneer.lanzoui.com/b06ugj5zc"})
+      进入子页面("调用",{标题="52《草东没有派对》",链接="https://lanzoui.com/b06ugj5zc"})
     end,--结束●52●
 
     function()--●53●
-      进入子页面("调用",{标题="53《熊熊作业》",链接="https://sneer.lanzoui.com/b06ugj9hi"})
+      进入子页面("调用",{标题="53《熊熊作业》",链接="https://lanzoui.com/b06ugj9hi"})
     end,--结束●53●
 
   })[id]()
