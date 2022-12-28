d850b5a555b19abf2043c1c76738a2b0cfd27cd2
嘲讽 <112706806+sneer0@users.noreply.github.com>
2022-12-28 15:58:50 +0800

    Update 云端.lua

--- 歌源/云端.lua
+++ 歌源/云端.lua
@@ -36,7 +36,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="智能线路●" then
       tbmx()--鸣谢
-      local txt="y-znxl"--●
+      local txt="yzn"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       弹出消息("正在重置页面 系统将智能切换最优线路")
       task(1000,function()--延迟1秒运行代码
@@ -46,7 +46,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❶" then
       tbmx()--鸣谢
-      local txt="y-1"--●
+      local txt="y1"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("https://2019334.xyz")
       弹出消息("已将线路1设为主页")
@@ -54,7 +54,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❷" then
       tbmx()--鸣谢
-      local txt="y-2"--●
+      local txt="y2"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("http://bbxxwl.com")
       弹出消息("已将线路2设为主页")
@@ -62,7 +62,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❸" then
       tbmx()--鸣谢
-      local txt="y-3"--●
+      local txt="y3"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("http://334.kim")
       弹出消息("已将线路3设为主页")
@@ -70,7 +70,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❹" then
       tbmx()--鸣谢
-      local txt="y-4"--●
+      local txt="y4"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("https://www.lizhi334.com/mp3/")
       弹出消息("已将线路4设为主页")
@@ -78,7 +78,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❺" then
       tbmx()--鸣谢
-      local txt="y-5"--●
+      local txt="y5"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("http://www.soulseeker.cn/?page_id=238")
       弹出消息("已将线路5设为主页")
@@ -86,7 +86,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❻" then
       tbmx()--鸣谢
-      local txt="y-6"--●
+      local txt="y6"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("https://lz.feifei.cf")
       弹出消息("已将线路6设为主页 点击右上角按钮播放")
@@ -94,7 +94,7 @@ function tbmx()--鸣谢开始♥
 
     if items[v+1]=="听歌线路❼" then
       tbmx()--鸣谢
-      local txt="y-7"--●
+      local txt="y7"--●
       io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
       加载网页("https://go.txca.ml/0:/lizhi/")
       弹出消息("已将线路7设为主页")
